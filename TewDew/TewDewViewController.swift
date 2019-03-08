08021853f11490e4753ec9d117654e1506865b50
Tony D <tdelgabsm@gmail.com>
2019-03-08 14:17:05 -0800

    first commit

---
+++ TewDew/TewDewViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  TewDew
+//
+//  Created by zvbsm on 2019/03/07.
+//  Copyright © 2019 Fernando Delgado. All rights reserved.
+//
+
+import UIKit
+
+class TewDewViewController: UITableViewController {
+    
+    let itemArray = ["Wake Up", "Write Code", "Eat Food"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TewDewItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
